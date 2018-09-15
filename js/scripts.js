15532c98265e6396aca4b8ab057ad612b8b255ae
hamida-mstafa <hamidamstafa@gmail.com>
2018-09-15 08:51:24 +0300

    add a js function

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,15 @@ function triangle(){
     return triangleDesign(a, b, c)
   } else {
     return "These sides do not make a triangle.";
+  }
+ };
+
+ var triangleDesign = function(a, b, c){
+   if (a===b && b===c && c===a) {
+     return "equilateral";
+   } else if (a===b || a===c || c===b) {
+     return "isosceles";
+   } else {
+     return "scalene";
+   }
+ };
